408f83ec34a1dd329b553556d35f4f0a70c2ec8c
modmax <modmax@abwesend.de>
2016-04-15 11:46:41 +0200

    Updated ofdb plugin

--- yamj3-plugin-ofdb/src/main/java/org/yamj/plugin/ofdb/OfdbMovieScanner.java
+++ yamj3-plugin-ofdb/src/main/java/org/yamj/plugin/ofdb/OfdbMovieScanner.java
@@ -1,3 +1,25 @@
+/*
+ *      Copyright (c) 2004-2016 YAMJ Members
+ *      https://github.com/organizations/YAMJ/teams
+ *
+ *      This file is part of the Yet Another Media Jukebox (YAMJ) plugins.
+ *
+ *      YAMJ is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      YAMJ is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with YAMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Web: https://github.com/YAMJ/yamj-v3-plugins
+ *
+ */
 package org.yamj.plugin.ofdb;
 
 import java.io.IOException;
@@ -79,7 +101,6 @@ public class OfdbMovieScanner implements MovieScanner {
 
         // add id in id map
         ids.put(SCANNER_NAME, ofdbUrl);
-        
         return ofdbUrl;
     }
 
@@ -323,8 +344,7 @@ public class OfdbMovieScanner implements MovieScanner {
     public String scanNFO(String nfoContent) {
         int beginIndex = nfoContent.indexOf("http://www.ofdb.de/film/");
         if (beginIndex != -1) {
-            StringTokenizer st = new StringTokenizer(nfoContent.substring(beginIndex), " \n\t\r\f!&é\"'(èçà)=$<>");
-            return st.nextToken();
+            return new StringTokenizer(nfoContent.substring(beginIndex), " \n\t\r\f!&é\"'(èçà)=$<>").nextToken();
         }
         return null;
     }
--- yamj3-plugin-ofdb/src/test/java/org/yamj/plugin/ofdb/OfdbMovieScannerTest.java
+++ yamj3-plugin-ofdb/src/test/java/org/yamj/plugin/ofdb/OfdbMovieScannerTest.java
@@ -1,8 +1,8 @@
 /*
- *      Copyright (c) 2004-2015 YAMJ Members
+ *      Copyright (c) 2004-2016 YAMJ Members
  *      https://github.com/organizations/YAMJ/teams
  *
- *      This file is part of the Yet Another Media Jukebox (YAMJ).
+ *      This file is part of the Yet Another Media Jukebox (YAMJ) plugins.
  *
  *      YAMJ is free software: you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
  *      You should have received a copy of the GNU General Public License
  *      along with YAMJ.  If not, see <http://www.gnu.org/licenses/>.
  *
- *      Web: https://github.com/YAMJ/yamj-v3
+ *      Web: https://github.com/YAMJ/yamj-v3-plugins
  *
  */
 package org.yamj.plugin.ofdb;
